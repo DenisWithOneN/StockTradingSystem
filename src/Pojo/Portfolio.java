f6bc495056667b106ad9a4fec37e501a6990b96c
DenisWithOneN <denis.ursut@emanuel.ro>
2024-01-29 20:38:14 +0200

    added portfolio methods add, remove, get

--- src/Pojo/Portfolio.java
+++ src/Pojo/Portfolio.java
@@ -1,4 +1,26 @@
 package Pojo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Portfolio {
+
+    private List<Stock> portfolioStocks;
+
+    public Portfolio() {
+        portfolioStocks = new ArrayList<>();
+    }
+
+
+    public List<Stock> getPortfolioStocks() {
+        return portfolioStocks;
+    }
+
+    public void addPortfolioStock(Stock stock) {
+        portfolioStocks.add(stock);
+    }
+
+    public void removePortfolioStock(Stock stock) {
+        portfolioStocks.remove(stock);
+    }
 }
